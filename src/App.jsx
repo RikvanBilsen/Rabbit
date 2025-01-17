6fe874fb72f9a02c4016321b0df795716aa9020d
rikva <rikvanb02@gmail.com>
2025-01-17 20:55:50 +0100

    changed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Home from './Pages/Home';
-import CreatePost from './Pages/CreatePost';
-import PostDetails from './Pages/PostDetails';
-import AccountPage from "./Pages/AccountPage.jsx";
-import LoginPage from "./Pages/LoginPage.jsx";
+import Header from './components/Header.jsx';
+import Sidebar from './components/Sidebar.jsx';
+import Home from './pages/Home.jsx';
+import CreatePost from './pages/CreatePost.jsx';
+import PostDetails from './pages/PostDetails.jsx';
+import AccountPage from "./pages/AccountPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
